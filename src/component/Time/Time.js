826f3223a3f8a2917417ff7b5b8e0aed8f52654d
masudrana-1 <masudrana58855@gmail.com>
2022-09-29 08:15:51 +0600

    add profile

--- src/component/Time/Time.js
+++ src/component/Time/Time.js
@@ -1,15 +1,49 @@
 import React from 'react';
+import './Time.css'
 
 const Time = () => {
     return (
         <div>
             <div className='profile-container'>
-                <img src="../../profile-img/masud.jpeg" alt="" />
-                <div>
-                    <h2>Masud Rana</h2>
-                    <p><small>Rangpur, Bangladesh</small></p>
+                <div className='profile'>
+                    <img src="../../profile-img/ZEZiR2FYeg.jpg" alt="" />
+                    <div>
+                        <h2>Masud Rana</h2>
+                        <p><small>Rangpur, Bangladesh</small></p>
+                    </div>
+                </div>
+                <div className='my-info'>
+                    <div>
+                        <h4>64kg</h4>
+                        <p>Weight</p>
+                    </div>
+                    <div>
+                        <h4>5.6"in</h4>
+                        <p>Height</p>
+                    </div>
+                    <div>
+                        <h4>25yrs</h4>
+                        <p>Age</p>
+                    </div>
                 </div>
             </div>
+            <div className='break-time'>
+                <h2>Take a break</h2>
+                <div>
+                    <p>5m</p>
+                    <p>10m</p>
+                    <p>15m</p>
+                    <p>20m</p>
+                </div>
+            </div>
+            <div>
+                <h2>Reading Details</h2>
+                <div>
+                    <p>Reading Time: </p>
+                    <p>Break Time: </p>
+                </div>
+            </div>
+            <button className='activity-btn'>Activity Completed</button>
         </div>
     );
 };
